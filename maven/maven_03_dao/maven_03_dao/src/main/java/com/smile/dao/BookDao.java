e90edb4aa860d01e4b7385b200893d12d4a84760
kiligsmile <kiligsmile@163.com>
2023-04-01 18:15:38 +0800

    spring study day5 maven

---
+++ maven/maven_03_dao/maven_03_dao/src/main/java/com/smile/dao/BookDao.java
@@ -0,0 +1,22 @@
+package com.smile.dao;
+
+import com.smile.domain.Book;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface BookDao {
+    @Insert("insert into tbl_book (type,name,description) values(#{type},#{name},#{description})")
+    public int save(Book book);
+    @Update("update tbl_book set type = #{type}, name = #{name}, description = #{description} where id = #{id}")
+    public int update(Book book);
+    @Delete("delete from tbl_book where id = #{id}")
+    public int delete(Integer id);
+    @Select("select * from tbl_book where id = #{id}")
+    public Book getById(Integer id);
+    @Select("select * from tbl_book")
+    public List<Book> getAll();
+}
